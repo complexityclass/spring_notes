ab2ecc333c48eabd69f5aeef6eff6d26dc2c064a
Valeriy Popov <complexityclass@yandex-team.ru>
2019-12-28 20:19:55 +0300

    Add empty spring project

---
+++ src/test/kotlin/com/springnotes/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springnotes.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
